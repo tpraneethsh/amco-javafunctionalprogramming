af3d6032ed23c7fc4a89e6e4c9a37013679e2b05
praneeththiruveedhula <tpraneethsh@gmail.com>
2023-05-18 18:39:13 +0200

    changes for Method Reference

--- src/main/java/com/sreeramtraders/ForEach.java
+++ src/main/java/com/sreeramtraders/ForEach.java
@@ -7,7 +7,12 @@ public class ForEach {
     public static void main(String[] args) {
         List<String> names = List.of("Pr","Bh","Ya","Sr");
 
-        Consumer<String> stringConsumer = name -> System.out.println(name);
-        names.forEach(stringConsumer);
+        //Consumer<String> stringConsumer = name -> System.out.println(name);
+        //names.forEach(stringConsumer);
+
+        //Method Reference
+        //System.out::println is MethodReference (Double colon::). In this below case (methodReference) we are not allowed to change the data of names.
+        Consumer<String> stringConsumer2 = System.out::println;
+        names.forEach(System.out::println);
     }
 }
