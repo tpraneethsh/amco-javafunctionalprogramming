43e89ed306729def7f691c2b54d533be6b59f8ce
praneeththiruveedhula <tpraneethsh@gmail.com>
2023-05-19 07:13:10 +0200

    Adding doubleFunction

--- src/main/java/com/sreeramtraders/FunctionImpl.java
+++ src/main/java/com/sreeramtraders/FunctionImpl.java
@@ -6,10 +6,6 @@ import java.util.stream.Collectors;
 
 public class FunctionImpl {
     public static void main(String[] args) {
-        System.out.println(incrementByOne(1));
-
-        System.out.println(incrementByOneFunc.apply(2));
-
         //List.of(1,2,3,4).forEach(n -> incrementByOne(n));
         //List.of(1,2,3,4).forEach(FunctionImpl::incrementByOne);
         List<Integer> intList = List.of(1,2,3,4)
@@ -18,10 +14,15 @@ public class FunctionImpl {
                 .map(FunctionImpl::incrementByOne)
                 .collect(Collectors.toList());
 
-        System.out.println(intList);
+        //System.out.println(incrementByOne(1));
+        //System.out.println(incrementByOneFunc.apply(2));
+        System.out.println(incrementByOneFunc.andThen(doubleFunc).apply(1));
+        //System.out.println(intList);
     }
 
     static Function<Integer, Integer> incrementByOneFunc = n -> n +1;
+
+    static Function<Integer, Integer> doubleFunc = n -> n * 2;
 
     static int incrementByOne(int n){
         return n +1;
